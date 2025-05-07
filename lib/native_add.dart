b89176cc57f00550753811d1803317d0b25d47e4
AoEiuV020 <aoeiuv020@gmail.com>
2025-05-07 15:40:33 +0800

    添加web支持
    实现暂用dart代码，

--- lib/native_add.dart
+++ lib/native_add.dart
@@ -1,30 +1,4 @@
-import 'dart:ffi';
-import 'dart:io';
+import 'native_add_io.dart' if (dart.library.html) 'native_add_web.dart'
+    as native_add;
 
-import 'generated_bindings.dart';
-
-/// A very short-lived native function.
-///
-/// For very short-lived functions, it is fine to call them on the main isolate.
-/// They will block the Dart execution while running the native function, so
-/// only do this for native functions which are guaranteed to be short-lived.
-int sum(int a, int b) => _bindings.sum(a, b);
-
-const String _libName = 'native_add';
-
-/// The dynamic library in which the symbols for [NativeAddBindings] can be found.
-final DynamicLibrary _dylib = () {
-  if (Platform.isMacOS || Platform.isIOS) {
-    return DynamicLibrary.open('$_libName.framework/$_libName');
-  }
-  if (Platform.isAndroid || Platform.isLinux) {
-    return DynamicLibrary.open('libsum.so');
-  }
-  if (Platform.isWindows) {
-    return DynamicLibrary.open('libsum.dll');
-  }
-  throw UnsupportedError('Unknown platform: ${Platform.operatingSystem}');
-}();
-
-/// The bindings to the native functions in [_dylib].
-final NativeLibrary _bindings = NativeLibrary(_dylib);
+int sum(int a, int b) => native_add.sum(a, b);
